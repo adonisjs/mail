142c847d14b9e0155f4de4358131d02a1a1423c8
Harminder Virk <virk.officials@gmail.com>
2020-12-02 14:27:38 +0530

    feat: add repl method to load mailers

--- src/Bindings/Repl.ts
+++ src/Bindings/Repl.ts
@@ -7,8 +7,19 @@
  * file that was distributed with this source code.
  */
 
+import { requireAll } from '@poppinss/utils'
 import { ReplContract } from '@ioc:Adonis/Addons/Repl'
 import { ApplicationContract } from '@ioc:Adonis/Core/Application'
+
+/**
+ * Helper to define REPL state
+ */
+function setupReplState(repl: ReplContract, key: string, value: any) {
+	repl.server.context[key] = value
+	repl.notify(
+		`Loaded ${key} module. You can access it using the "${repl.colors.underline(key)}" variable`
+	)
+}
 
 /**
  * Defune repl bindings. The method must be invoked when application environment
@@ -16,17 +27,26 @@ import { ApplicationContract } from '@ioc:Adonis/Core/Application'
  */
 export function defineReplBindings(application: ApplicationContract, Repl: ReplContract) {
 	Repl.addMethod(
-		'loadMailer',
+		'loadMail',
 		(repl) => {
-			repl.server.context.Mail = application.container.use('Adonis/Addons/Mail')
-			repl.notify(
-				`Loaded Mail module. You can access it using the "${repl.colors.underline(
-					'Mail'
-				)}" variable`
-			)
+			setupReplState(repl, 'Mail', application.container.use('Adonis/Addons/Mail'))
 		},
 		{
 			description: 'Load mail provider and save reference to the "Mail" variable',
+		}
+	)
+
+	Repl.addMethod(
+		'loadMailers',
+		(repl) => {
+			const mailersPath = application.resolveNamespaceDirectory('mailers') || 'app/Mailers'
+			console.log(repl.colors.dim(`recursively reading mailers from "${mailersPath}"`))
+
+			const mailerAbsPath = application.makePath(mailersPath)
+			setupReplState(repl, 'mailers', requireAll(mailerAbsPath))
+		},
+		{
+			description: 'Recursively loads Mailers to the "mailers" property',
 		}
 	)
 }
