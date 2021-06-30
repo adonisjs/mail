ad9784920fd3bac1b33f5076337381a3c0680915
Chad Pitchford <cjpitch23@gmail.com>
2021-06-30 09:46:51 -0500

    fix: forward all recipients to mailgun under to field
    * Fixed issue with Mailgun driver
    Added bcc and cc to 'to' for MIME
    
    * Fixed linting issue with semi colon
    
    * Changed code to append all recipients to the 'to' param
    
    Co-authored-by: cjpitch23 <chad.pitchford@potawatomi.org>

--- src/Transports/Mailgun.ts
+++ src/Transports/Mailgun.ts
@@ -108,7 +108,8 @@ export class MailgunTransport {
 
     Object.keys(tags).forEach((key) => form.append(key, tags[key]))
     Object.keys(headers).forEach((key) => form.append(key, headers[key]))
-    Object.keys(recipients).forEach((key) => form.append(key, recipients[key]))
+    Object.keys(recipients).forEach((key) => form.append('to', recipients[key]))
+
     form.append('message', mail.message.createReadStream(), { filename: 'message.mime' })
 
     this.logger.trace(
